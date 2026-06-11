62f2af1f0a646c716c85e38166bcf891db21a819
ANUSHA REDDAMONI <162695046+Anushareddamoni@users.noreply.github.com>
2026-06-11 13:09:45 +0530

    Add files via upload

---
+++ statements.py
@@ -0,0 +1,16 @@
+weather="sunny"
+time_of_day="night"
+if (weather=="sunny"):
+    if time_of_day=="day":
+        print("you play with your car toy.")
+    else:
+        print("it's night.time to sleep.")
+elif(weather=="rainy"):
+    print("you play with your boat toy.")
+elif(weather=="snowyy"):
+    if(time_of_day=="night"):
+        print("you play with your teddy bear toy.")
+    else:
+        print("you play with your snowman toy.")
+else:
+    print("you stay inside and read a storybook.")                            
